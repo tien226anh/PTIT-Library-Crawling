69f5857e6380c69ba052e48edb9b9581e03d8c98
tien226anh <tien226anh@gmail.com>
2023-02-06 15:57:23 +0700

    add project

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Cách sử dụng</h1>
+<h1>How to use</h1>
+<br/>
+Mở folder này trong VSCode hoặc IDE hỗ trợ python và nhập dòng lệnh sau vào TERMINAL: 
+pip install -r requirements.txt
+<br/>
+Cài Chrome Webdriver, giải nén ra và đặt file .exe vào folder này
+<br/>
+Chạy câu lênh sau trong terminal để chạy chương trình:
+python main.py
+<br/>
+hoặc
+<br/>
+python3 main.py
---
+++ main.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
+from time import sleep
+
+import pyperclip
+import urllib.request
+
+import bs4
+
+# No close browser when finish test and full-screen browser
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+chrome_options.add_argument("--start-maximized")
+
+
+# driver = webdriver.Chrome(chrome_options=chrome_options)
+# driver = webdriver.Firefox()
+# driver.get("http://dlib.ptit.edu.vn/flowpaper/simple_document.php?subfolder=30/16/21/&doc=30162126041710350305223424440233135951&bitsid=dd544e34-2a5e-4c19-841f-38f79db31fad&uid=869e977e-9ed5-4588-8f4c-f169046ea281")
+
+def login():
+    # username='b20dccn005'
+    # password='b20dccn005'
+    # inputUsername = driver.find_element_by_id("tlogin_email")
+    inputUsername = driver.find_element(By.ID, "tlogin_email")
+    inputUsername.send_keys(username)
+    inputPassword = driver.find_element(By.ID, "tlogin_password")
+    inputPassword.send_keys(password)
+    inputPassword.send_keys(Keys.ENTER)
+    
+def download():
+    elem = driver.find_element(By.ID, "page_0_documentViewer")
+    src = elem.get_attribute("src")
+    print(src)
+    div_count = driver.find_elements(By.CLASS_NAME, "flowpaper_page")
+    count = len(div_count)
+    src = src.replace("page=1", "page=n")
+    print("new src: ", src)
+    for i in range(count):
+        page = "page="+str(i+1)
+        new_src = src.replace("page=n", page)
+        location = "D:/Workspace/LibScraping/imgdl/"+str(i+1)+".jpg"
+        urllib.request.urlretrieve(new_src, location)
+        
+
+link = input("Mời bạn nhập trang sách thư viện bạn muốn crawl về: ")
+username = input("Mời bạn tài khoản: ")
+password = input("Mời bạn nhập mật khẩu: ")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get(link)
+sleep(3)
+login()
+sleep(7)
+download()
+
+sleep(5)
+driver.close()
